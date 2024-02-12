2244eead11a9131fe103d92ebf7f19f1409ef2ee
Sem Kluiver <semkluiver@xs4all.nl>
2024-02-12 14:11:20 +0100

    Make sure only actual tiles are extracted from tile sheet. Also make sure only unique tiles are saved. Duplicates are removed.
    Co-authored-by: Per Hermanus <per.hermanus@gmail.com>

--- tileGenerator.py
+++ tileGenerator.py
@@ -8,6 +8,7 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 import os
+import copy
 
 
 def read_args(argflag):
@@ -29,21 +30,21 @@ def guess_gridsize():
     pass
     # return tileSizePx, gridSizePx
 
+
 # This function creates a black and white mask for the tileset.
 # The color of the grid is auto-detected, after a mask, removing
 # the grid, is created. Then, we detect all contours of the mask.
 # Using these contours, we cut out each tile and save it as a
-# separate image.
-
-# TODO: make sure only the largest area of the detected grid color is
-# put in the mask. See: https://z-uo.medium.com/opencv-automatic-select-big-contour-areas-and-remove-8d79464a06e7
-# Also, remove the background of tiles that have transparency. Now, they take over the background color.
-# Do some research into the alpha in openCV.
+# separate image. Note that this only works if the tileset does
+# not contain the color of the mask itself, although we think
+# this is safe to assume.
+# The grid also needs to go around the border of the whole image.
+# We use the top left pixel to detect the color of the mask.
 def separate_grid(filename, tileset):
     # Detect grid color
     gridcolor = tileset[0,0]
 
-    # Create mask
+    # Create the mask.
     mask = np.all(tileset == gridcolor, axis=-1)
     mask = ~mask
     color_mask = mask.astype(np.uint8)
@@ -54,12 +55,27 @@ def separate_grid(filename, tileset):
     cv.imwrite("./masks/{f}_mask.png".format(f=filename), color_mask)
 
     # Detect contours
-    contours, hierarchy = cv.findContours(image=color_mask, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv.findContours(image=color_mask, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_SIMPLE)
+
+    # First pass: check most common area
+    area_dict = {}
+    for c in contours:
+        area = cv.contourArea(c)
+        if area not in area_dict:
+            area_dict[area] = 1
+        else:
+            area_dict[area] += 1
+
+    # Determine the standard area: the most-occurring area of tiles.
+    # We discard tiles with other dimensions, as they will not fit.
+    standard_area = max(zip(area_dict.values(), area_dict.keys()))[1]
+
+    tiles_list = [] # set()
+    tiles_hash = set()
 
     # For all contours, cut out tile
     imgname = 0
     for c in contours:
-        # Detect contour corners
         maxX, maxY = -1, -1
         minX = tileset.shape[:2][1]
         minY = tileset.shape[:2][0]
@@ -74,18 +90,36 @@ def separate_grid(filename, tileset):
             if coord[1] < minY: #y coord
                 minY = coord[1]
 
+        # Check if tile is a square. If not, we skip the tile.
+        if maxX - minX != maxY - minY:
+            continue
+
+        # If area of contour does not match standard area, we skip this tile.
+        # We may lose some tiles, bat that's a sacrifice we're willing to make. Tey would probably not fit with the
+        # other tiles anyway.
+        if cv.contourArea(c) != standard_area:
+            continue
+
         box_image = tileset[minY : maxY+1, minX: maxX+1]
 
-        os.makedirs("./{f}_tiles".format(f=filename), exist_ok=True)
-        
+        # Hash the tile to ensure we only save unique tiles
+        if hash(str(box_image)) not in tiles_hash:
+            tiles_hash.add(hash(str(box_image)))
+            tiles_list.append(box_image)     
+
+    # Create folder for tiles
+    os.makedirs("./{f}_tiles".format(f=filename), exist_ok=True)
+    
+    # Write all (unique) tiles
+    imgname = 0
+    for tile in tiles_list:
         try:
-            cv.imwrite("./{f}_tiles/{i}.png".format(f=filename, i=imgname), box_image)
+            cv.imwrite("./{f}_tiles/{i}.png".format(f=filename, i=imgname), tile)
             imgname += 1
 
         except:
             raise BufferError("Error: Tried to save an empty image.")
 
-    # return seperate_tiles_list
 
 def main():
     #load tileset
@@ -100,13 +134,6 @@ def main():
     # if read_args("-g") != 
     tileSizePx = read_args("-t") #-1 if not specified
     gridSizePx = read_args("-g")
-
-    #seperate tiles
-    
-    
-
-
-    pass
 
 if __name__=="__main__": 
     main() 
