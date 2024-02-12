c7fc0d996f7c050214441b1c000925d877073922
Sem Kluiver <semkluiver@xs4all.nl>
2024-02-12 15:53:56 +0100

    Add terminal options to provide a hardcoded offset for tilesheets. Some tilesheets do not have grids, and this will split those accordingly. No smart trickery, though. It just cuts the image in pieces of SxS, where S is the provided size of the tile.
    Co-authored-by: Per Hermanus <per.hermanus@gmail.com>

--- tileGenerator.py
+++ tileGenerator.py
@@ -13,8 +13,8 @@ import copy
 
 def read_args(argflag):
     for i in sys.argv[2:]:
-        x = x.split
-        if (x.split('=')[0] == argflag):
+        x = i.split('=')
+        if (x[0] == argflag):
             return int(x[1])
     return -1
     
@@ -30,6 +30,26 @@ def guess_gridsize():
     pass
     # return tileSizePx, gridSizePx
 
+# adds a tile to a list. If an accompanyin hashset is passed it doesn't add duplicates
+def add_tile(tile, tilelist, hashset=None):
+    if hashset is None:
+        tilelist.append(tile)
+    else:
+        if hash(str(tile)) not in hashset:
+            hashset.add(hash(str(tile)))
+            tilelist.append(tile)
+    
+def write_tiles(file_dir, tilelist):
+    os.makedirs("./{f}_tiles".format(f=file_dir), exist_ok=True)
+    imgname = 0
+    for tile in tilelist:
+        try:
+            cv.imwrite("./{f}_tiles/{i}.png".format(f=file_dir, i=imgname), tile)
+            imgname += 1
+
+        except:
+            raise BufferError("Error: Tried to save an empty image.")
+    
 
 # This function creates a black and white mask for the tileset.
 # The color of the grid is auto-detected, after a mask, removing
@@ -70,7 +90,7 @@ def separate_grid(filename, tileset):
     # We discard tiles with other dimensions, as they will not fit.
     standard_area = max(zip(area_dict.values(), area_dict.keys()))[1]
 
-    tiles_list = [] # set()
+    tiles_list = []
     tiles_hash = set()
 
     # For all contours, cut out tile
@@ -103,22 +123,31 @@ def separate_grid(filename, tileset):
         box_image = tileset[minY : maxY+1, minX: maxX+1]
 
         # Hash the tile to ensure we only save unique tiles
-        if hash(str(box_image)) not in tiles_hash:
-            tiles_hash.add(hash(str(box_image)))
-            tiles_list.append(box_image)     
-
-    # Create folder for tiles
-    os.makedirs("./{f}_tiles".format(f=filename), exist_ok=True)
+        add_tile(box_image, tiles_list, tiles_hash)
     
-    # Write all (unique) tiles
-    imgname = 0
-    for tile in tiles_list:
-        try:
-            cv.imwrite("./{f}_tiles/{i}.png".format(f=filename, i=imgname), tile)
-            imgname += 1
+    
+    write_tiles(filename, tiles_list)
 
-        except:
-            raise BufferError("Error: Tried to save an empty image.")
+
+# "Hardcoded" tile extraction, for tilesets that contain no grid lines.
+# tilesize denotes the standard size of the tiles (16 means 16x16).
+# Offset is for the cases where there is a border around the entire tileset,
+# but no grid.
+def fixed_offset_extraction(filename, tileset, tilesize, offset):
+    x = 0
+    y = 0
+    tilesetX_size = tileset.shape[:2][1]
+    tilesetY_size = tileset.shape[:2][0]
+    tiles_list = []
+    tiles_hash = set()
+    print(tilesetX_size, tilesetY_size)
+    for x in range(offset, tilesetX_size, tilesize):
+        for y in range(offset, tilesetY_size, tilesize):
+            box_image = tileset[y : y+tilesize, x : x+tilesize]
+            add_tile(box_image, tiles_list, tiles_hash)
+
+    write_tiles(filename, tiles_list)
+    
 
 
 def main():
@@ -128,12 +157,21 @@ def main():
     filename = os.path.basename(filename_path).split(".")[0]
     print("Filename:" , filename)
 
-    separate_grid(filename, tileset)
+    
 
     #if grid, detect distance between tiles
     # if read_args("-g") != 
     tileSizePx = read_args("-t") #-1 if not specified
     gridSizePx = read_args("-g")
+    borderSizePx = read_args("-b")
+
+    if borderSizePx == -1 and tileSizePx == -1:
+        separate_grid(filename, tileset)
+    else:
+        print("manual mode")
+        fixed_offset_extraction(filename, tileset, tileSizePx, borderSizePx)
+
+    
 
 if __name__=="__main__": 
     main() 
