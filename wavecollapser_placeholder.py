2330bd39a2396e13098778c22c9a83ec20b26fa4
Sem Kluiver <semkluiver@xs4all.nl>
2024-03-25 15:32:25 +0100

    Add recursive DFS to generate the world. Now the algorithm rarely gets stuck.
    Co-authored-by: Per Hermanus <per.hermanus@gmail.com>

--- wavecollapser_placeholder.py
+++ wavecollapser_placeholder.py
@@ -47,20 +47,16 @@ class World:
                 world[x].append(Tile(x, y, list(self.neighbor_rules.keys())))
                 
         self.world = world
-
-    # # Returns a tile at position x, y
-    # def get_tile(self, x, y):
-    #     return self.world[x][y]
     
-    # Selects the tile with the lowest entropy
-    def select_tile(self):
+    # Finds the candidate tiles with the lowest entropy
+    def find_candidate_tiles(self):
         lowest_entropy = len(self.neighbor_rules.keys())
         candidate_tiles = []
         for x in range(self.xSize):
             for y in range (self.ySize):
                 tile = self.world[x][y]
                 if tile.get_tile_id() == "-1":
-                    self.world[x][y].update_entropy()
+                    tile.update_entropy()
                     # Entropy is lower. Remove candidate tiles and continue with
                     # new lowest entropy
                     if tile.get_entropy() < lowest_entropy:
@@ -70,37 +66,25 @@ class World:
                     elif tile.get_entropy() == lowest_entropy:
                         candidate_tiles.append(tile)
 
-        chosen_tile = 0
-        while len(candidate_tiles) > 0:
-            chosen_tile = choice(candidate_tiles)
-            candidate_tiles.remove(chosen_tile)
-            candidate_tile_options = copy.deepcopy(chosen_tile.get_possibilities())
-            while len(candidate_tile_options) > 0:
-                id_to_check = choice(list(candidate_tile_options))
-                candidate_tile_options.remove(id_to_check)
-                posx, posy = chosen_tile.get_position()
-                if self.check_nb_viable(posx, posy, id_to_check):
-                    return chosen_tile, id_to_check
-        else:
-            return None, None
+        return candidate_tiles
         
     #checks if given a certain tile id on a position, no neighbour gets no viable possibilities
     def check_nb_viable(self, x, y, id):
-        if (y-1 > 0): # if above is in bounds
+        if (y-1 >= 0): # if above is in bounds
             # up
-            if (len(self.world[x][y-1].get_possibilities() & self.get_nb_possibilities(id, "up")) <= 0):
+            if (len(self.world[x][y-1].get_possibilities() & self.get_nb_possibilities(id, "up")) <= 0) and self.world[x][y-1].get_tile_id() == "-1":
                 return False
         if (x+1 < self.xSize):
             # right
-            if (len(self.world[x+1][y].get_possibilities() & self.get_nb_possibilities(id, "right")) <= 0):
+            if (len(self.world[x+1][y].get_possibilities() & self.get_nb_possibilities(id, "right")) <= 0) and self.world[x+1][y].get_tile_id() == "-1":
                 return False
         if (y+1 < self.ySize):
             # down
-            if (len(self.world[x][y+1].get_possibilities() & self.get_nb_possibilities(id, "down")) <= 0):
+            if (len(self.world[x][y+1].get_possibilities() & self.get_nb_possibilities(id, "down")) <= 0) and self.world[x][y+1].get_tile_id() == "-1":
                 return False
-        if (x-1 > 0):
+        if (x-1 >= 0):
             # left
-            if (len(self.world[x-1][y].get_possibilities() & self.get_nb_possibilities(id, "left")) <= 0):
+            if (len(self.world[x-1][y].get_possibilities() & self.get_nb_possibilities(id, "left")) <= 0) and self.world[x-1][y].get_tile_id() == "-1":
                 return False
         return True
         
@@ -113,7 +97,7 @@ class World:
         for x in range(self.xSize):
             for y in range(self.ySize):
                 if self.world[x][y].get_tile_id() != "-1":
-                    if y-1 > 0 : # if above is in bounds
+                    if y-1 >= 0 : # if above is in bounds
                         # up
                         self.world[x][y-1].set_possibilities(self.world[x][y-1].get_possibilities() & 
                         self.get_nb_possibilities(self.world[x][y].get_tile_id(), "up")) 
@@ -125,7 +109,7 @@ class World:
                         # down
                         self.world[x][y+1].set_possibilities(self.world[x][y+1].get_possibilities() & 
                                                                 self.get_nb_possibilities(self.world[x][y].get_tile_id(), "down"))
-                    if x-1 > 0:
+                    if x-1 >= 0:
                         # left
                         self.world[x-1][y].set_possibilities(self.world[x-1][y].get_possibilities() & 
                                                                 self.get_nb_possibilities(self.world[x][y].get_tile_id(), "left"))
@@ -171,12 +155,31 @@ class World:
                 
         return False
     
+    # Walk through all tiles and check whether we are done.
+    # We are done when all tiles have an id that is not -1
+    # and the entropy is 0.
+    def done_check(self):
+        for x in range(self.xSize):
+            for y in range(self.ySize):
+                if self.world[x][y].get_tile_id() == "-1" or self.world[x][y].get_entropy() != 0:
+                    return False
+                
+        return True
+    
     # Print all ids in the world in terminal.
     def debug_terminal_print(self):
         for y in range(self.ySize):
             for x in range(self.xSize):
                 print(self.world[x][y].get_tile_id(), end="" + '\t')
             print("\n", end="")
+
+
+    def debug_terminal_print_entropy(self):
+        for y in range(self.ySize):
+            for x in range(self.xSize):
+                print(self.world[x][y].get_entropy(), end="" + '\t')
+            print("\n", end="")
+
 
 
 
@@ -203,7 +206,8 @@ class Tile:
     
     # Set the possibilities of the tile.
     def set_possibilities(self, new_possibilities):
-        self.possibilities = new_possibilities
+        self.possibilities = copy.deepcopy(new_possibilities)
+        self.update_entropy()
     
     # Returns the type id of the tile.
     def get_tile_id(self):
@@ -212,6 +216,9 @@ class Tile:
     # Set the type id of the tile.
     def set_tile_id(self, id):
         self.tile_id = id
+        if id != '-1':
+            self.set_possibilities(set())
+            self.update_entropy()
 
     # Returns the position of the tile in the world.
     def get_position(self):
@@ -232,6 +239,48 @@ def load_neighbors_json(filename):
 
             
 
+def generate(world):
+    # Main generating loop
+    while(True):
+        if world.stuck_check():
+            print("We are stuck!")
+            return None
+        candidate_tiles = world.find_candidate_tiles()
+    
+        while(True):
+            if world == None:
+                print("Hard error bro")
+            if world.done_check():
+                return world
+            if (len(candidate_tiles) == 0):
+                print("No more candidate tiles in this branch, going back")
+                return None
+            chosen_tile = choice(list(candidate_tiles))
+            candidate_tiles.remove(chosen_tile)
+
+            candidate_tile_ids = chosen_tile.get_possibilities()
+            if len(candidate_tile_ids) > 0:
+                chosen_tile_id = choice(list(candidate_tile_ids))
+                candidate_tile_ids.remove(chosen_tile_id)
+
+                chosen_tile.set_tile_id(chosen_tile_id)
+                world.collapse()
+                
+                # print("Current world")
+                # world.debug_terminal_print()
+                # print("Current entropy")
+                # world.debug_terminal_print_entropy()
+                # print()
+
+                done_world = generate(copy.deepcopy(world))
+                if done_world != None and done_world.done_check():
+                    print("We are done")
+                    world = done_world
+            else:
+                print("No more ids available for candidate tile, choosing another")
+                
+    
+
 
 def main():
     tilesname = input("What tileset to use for world generation? ")
@@ -243,40 +292,32 @@ def main():
     all_tile_ids = list(tile_imgs.keys())
     world = World(xSize, ySize, tile_neighbors)
 
-    # Main generating loop
-    while(True):
-        new_tile, new_id = world.select_tile()
-        if new_tile == None:
-            print("Finished!")
-            world.create_image(tile_imgs)
-            break
-        else:
-            # possible_tiles = list(new_tile.get_possibilities())
-            while(True):
-                # chosen_tile = choice(possible_tiles)
-                new_tile.set_tile_id(new_id)
-                world.history.append(new_tile) # put tile on stack
-                world.collapse()
-                if not world.stuck_check():
-                    print("Continue")
-                    break
-                else:
-                    print("LALALA")
-                    raise BufferError("This is not a buffer error but we're stuck")
-                    # # Undo move and remove this tile from possibilities
-                    # print("Undo tile...")
-                    # new_tile.set_tile_id(-1)
-                    # world.history.pop()
-                    # possible_tiles -= [chosen_tile]
-                    # # We need to reset the possibilities in the world since
-                    # # we had to undo a move. After resetting, we recompute
-                    # # the actual possibilities using a collapse.
-                    # world.reset_possibilities()
-                    # world.collapse()
+    world = generate(world)
+
+    world.create_image(tile_imgs)
 
 
-        world.debug_terminal_print()
-        print()
+        
+
+    
 
 if __name__=="__main__": 
     main()
+
+"""
+TODO:
+Er zijn tegels die geen buren kunnen hebben aan een zijde. Wanneer die als eerste
+geplaatst worden, loopt het geheel vast als die niet aan de rand van het bord staan,
+omdat er een kant is waar geen enkele mogelijke buur is.
+
+Soms duurt genereren heel erg lang, misschien door moeilijke tegels.
+Misschien een soort stop-moment inbouwen?
+
+Water fixen: tegels die niet in de wereld voorkomen wel alloceren zodat die gevonden kunnen
+worden, waaronder water.
+
+Gewichten zodat we niet 3 telefoonhuisjes krijgen
+
+Misschien ook nog als extensie: kijken tussen world-sections, maar niet altijd.
+Alleen als het in dezelfde type "biome" is
+"""
