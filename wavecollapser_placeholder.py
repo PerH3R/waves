da4c008c220bd30325b7f8062b056880401962a7
Per Hermanus <per.hermanus@gmail.com>
2024-07-23 17:33:27 +0200

    see last one

--- wavecollapser_placeholder.py
+++ wavecollapser_placeholder.py
@@ -226,24 +226,24 @@ class World:
                     if y-1 >= 0 : # if above is in bounds
                         # up
                         # processes.append(Process(target=self.collapse_tile, args=(self.world[x][y-1], 
-                                                                             self.get_nb_possibilities(self.world[x][y].get_tile_id(), "up"))
-                                                ))
+                        #                                                      self.get_nb_possibilities(self.world[x][y].get_tile_id(), "up"))
+                        #                         ))
                         # self.world[x][y-1].set_possibilities(self.world[x][y-1].get_possibilities() & 
                         #     self.get_nb_possibilities(self.world[x][y].get_tile_id(), "up"))
                         self.collapse_tile(self.world[x][y-1], self.get_nb_possibilities(self.world[x][y].get_tile_id(), "up"))
                     if x+1 < self.xSize:
                         # right
                         # processes.append(Process(target=self.collapse_tile, args=(self.world[x+1][y], 
-                                                                             self.get_nb_possibilities(self.world[x][y].get_tile_id(), "right"))
-                                                ))
+                        #                                                      self.get_nb_possibilities(self.world[x][y].get_tile_id(), "right"))
+                        #                         ))
                         # self.world[x+1][y].set_possibilities(self.world[x+1][y].get_possibilities() & 
                         #     self.get_nb_possibilities(self.world[x][y].get_tile_id(), "right"))
                         self.collapse_tile(self.world[x+1][y], self.get_nb_possibilities(self.world[x][y].get_tile_id(), "right"))
                     if y+1 < self.ySize:
                         # down
                         # processes.append(Process(target=self.collapse_tile, args=(self.world[x][y+1], 
-                                                                             self.get_nb_possibilities(self.world[x][y].get_tile_id(), "down"))
-                                                ))
+                        #                                                      self.get_nb_possibilities(self.world[x][y].get_tile_id(), "down"))
+                        #                         ))
                         # self.world[x][y+1].set_possibilities(self.world[x][y+1].get_possibilities() & 
                         #     self.get_nb_possibilities(self.world[x][y].get_tile_id(), "down"))
                         self.collapse_tile(self.world[x][y+1], self.get_nb_possibilities(self.world[x][y].get_tile_id(), "down"))
@@ -251,8 +251,8 @@ class World:
                     if x-1 >= 0:
                         # left
                         # processes.append(Process(target=self.collapse_tile, args=(self.world[x-1][y], 
-                                                                             self.get_nb_possibilities(self.world[x][y].get_tile_id(), "left"))
-                                                ))
+                        #                                                      self.get_nb_possibilities(self.world[x][y].get_tile_id(), "left"))
+                        #                         ))
                         # self.world[x-1][y].set_possibilities(self.world[x-1][y].get_possibilities() & 
                         #     self.get_nb_possibilities(self.world[x][y].get_tile_id(), "left"))
                         self.collapse_tile(self.world[x-1][y], self.get_nb_possibilities(self.world[x][y].get_tile_id(), "left"))
